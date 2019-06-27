6ff5caebfe9e5cd0c1bf2e271bd65e436ae79afe
bolajiayodeji <sirbeejay1@gmail.com>
2019-06-27 01:29:48 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ Component } from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      items: [],
+      currrentItem: {text: '', key: ''},
+    }
+  }
+  handleInput = (e) => {
+    console.log('test')
+  }
+  addItem = (e) => {
+    e.preventDefault();
+    console.log('addddd')
+  }
+  render() {
+    return (
+      <div className="App">
+        <TodoList addItem={this.addItem} />
+      </div>
+    );
+  }
 }
 
 export default App;
